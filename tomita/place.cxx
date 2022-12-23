8027ba9b4b17ca632ade95f47521afbb09cef2ff
Andrey Malkov <108325837+anmalk@users.noreply.github.com>
2022-12-23 20:37:45 +0300

    Add files via upload

---
+++ tomita/place.cxx
@@ -0,0 +1,4 @@
+#encoding "utf8"
+PlaceName -> Word<kwtype=dostopr>;
+
+Place -> PlaceName interp (Place.Name);
---
+++ tomita/politic.cxx
@@ -0,0 +1,5 @@
+#encoding "utf8"
+PolitName -> Word<kwtype=politiki>;
+
+Polit -> PolitName interp (Polit.Surname);
+
